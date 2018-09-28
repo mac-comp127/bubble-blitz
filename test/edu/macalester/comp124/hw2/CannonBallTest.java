aa3ac06dbd6d8dee4204f2a63d804ff30bd6e812
Paul Cantrell <cantrell@pobox.com>
2018-09-27 22:40:18 -0500

    Updated test for iterative physics

--- test/edu/macalester/comp124/hw2/CannonBallTest.java
+++ test/edu/macalester/comp124/hw2/CannonBallTest.java
@@ -8,21 +8,22 @@ import static org.junit.Assert.assertTrue;
 
 public class CannonBallTest {
 
-//    @Test
-//    public void testBasicUpdatePosition(){
-//        //Assumes parameter order is x, y, velocity, angleInDegrees, maxXBound, maxYBound. Modify if your constructor is different.
-//        CannonBall ball = new CannonBall(100,100, 100, 45, 1100, 600);
-//
-//        ball.updatePosition();
-//        assertEquals(104.57106781186548, ball.getX(), 0.001);
-//        assertEquals(90.47793218813453, ball.getY(), 0.001);
-//        ball.updatePosition();
-//        assertEquals(111.64213562373095, ball.getX(), 0.001);
-//        assertEquals(83.55386437626905, ball.getY(), 0.001);
-//        ball.updatePosition();
-//        assertEquals(118.71320343559643, ball.getX(), 0.001);
-//        assertEquals(76.72779656440358, ball.getY(), 0.001);
-//    }
+    @Test
+    public void testBasicUpdatePosition(){
+        CannonBall ball = new CannonBall(100, 100, 100, 45, 1100, 600);
+
+        ball.updatePosition(0.1);
+        assertEquals(104.571, ball.getX(), 0.001);
+        assertEquals(90.4289, ball.getY(), 0.001);
+
+        ball.updatePosition(0.1);
+        assertEquals(111.642, ball.getX(), 0.001);
+        assertEquals(83.4558, ball.getY(), 0.001);
+
+        ball.updatePosition(0.1);
+        assertEquals(118.713, ball.getX(), 0.001);
+        assertEquals(76.5807, ball.getY(), 0.001);
+    }
 
     //TODO: Add more test methods
 }
