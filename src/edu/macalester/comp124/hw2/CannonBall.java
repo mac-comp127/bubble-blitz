a6104e16d7f4b726423ca87da62d468e442808e8
Paul Cantrell <cantrell@pobox.com>
2018-09-27 22:13:49 -0500

    Provided CannonBall constructor signature

--- src/edu/macalester/comp124/hw2/CannonBall.java
+++ src/edu/macalester/comp124/hw2/CannonBall.java
@@ -9,13 +9,32 @@ public class CannonBall extends Ellipse {
     public static final double GRAVITY = -9.8;
     public static final double BALL_RADIUS = 2.5;
 
-    //TODO: Add a constructor and instance variables
+    //TODO: Add instance variables. Remember that instance variables may or may not have corresponding constructor parameters.
+
+    public CannonBall(
+            double centerX,
+            double centerY,
+            double initialSpeed,
+            double initialAngle,
+            double maxX,
+            double maxY) {
+
+        //TODO: implement me
+
+        // To compute the initial velocity:
+        //
+        //double initialAngleRadians = Math.toRadians(initialAngle);
+        //initialSpeed * cos(initialAngleInRadians)   // initial x velocity
+        //initialSpeed * -sin(initialAngleInRadians)  // initial y velocity
+        //
+        // (You'll need to figure out how to use those values.)
+    }
 
     /**
      * Update the cannon ball's position if it is in bounds
      * @return true if the ball is in within the maxXBound and maxYBound
      */
-    public boolean updatePosition(){
+    public boolean updatePosition() {
         //TODO: fix me
         return false;
     }
